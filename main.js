58e7b563d143febb9f2ddd6cd8dec2bfb3c91117
simonestrinati <simone.strinati@outlook.it>
2025-11-14 18:08:46 +0100

    preparazione variabili ed Event

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-document.getElementById("submit-btn").addEventListener("click", function (event) {
+const distanceEl = document.getElementById("distance");
+const ageEl = document.getElementById("age-field");
+const submitBtnEl = document.getElementById("submit-btn");
+const finalPriceEl = document.getElementById("finalPrice");
+const priceKm = 0.21;
+
+submitBtnEl.addEventListener("click", function (event) {
 
     event.preventDefault();
-    console.log("Cliccato!");
+    
+    let prezzoBase = parseInt(distanceEl) * priceKm;
+
 
 
 });
